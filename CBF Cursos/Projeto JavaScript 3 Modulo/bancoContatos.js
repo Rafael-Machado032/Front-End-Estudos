64f7f48d557fd71ab7bea5ea563401d7f62279ed
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-02-27 23:41:40 -0300

    atualização

--- CBF Cursos/Projeto JavaScript 3 Modulo/bancoContatos.js
+++ CBF Cursos/Projeto JavaScript 3 Modulo/bancoContatos.js
@@ -1,2 +1,2 @@
-let contatos=[]
-export {contatos}
+let contatos=[] //Array onde vai ser armazenado os contatos
+export {contatos} //Esporta para contatos
--- CBF Cursos/Projeto JavaScript 3 Modulo/script.js
+++ CBF Cursos/Projeto JavaScript 3 Modulo/script.js
@@ -1,13 +1,14 @@
-import c from "./contatos.js"
-const listaContatos=document.getElementById("listaContatos")
+import c from "./contatos.js" //importou default renomeou para c
+
+const listaContatos=document.getElementById("listaContatos") //Pegou o elemento com id listaContatos e botou no const
 const btn_gravar=document.getElementById("btn_gravar")
 
-btn_gravar.addEventListener("click",(evt)=>{
-    const cont={
-        nome:document.getElementById("f_nome").value,
+btn_gravar.addEventListener("click",(evt)=>{ //Trata o evento de click
+    const cont={ //Insere os dados digitados nesse const
+        nome:document.getElementById("f_nome").value, //Pega o valor dos campos digitado
         telefone:document.getElementById("f_telefone").value,
         email:document.getElementById("f_email").value
     }
-    c.addContato(cont,listaContatos)
+    c.addContato(cont,listaContatos) //chama a função addContato com os dados e onde vai colocar
 
 })
