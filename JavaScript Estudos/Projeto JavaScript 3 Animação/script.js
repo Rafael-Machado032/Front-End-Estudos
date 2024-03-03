122c7428ab073d423f54f0401ecea3bd82f630b3
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2024-03-02 22:07:31 -0300

    a

---
+++ JavaScript Estudos/Projeto JavaScript 3 Animação/script.js
@@ -0,0 +1,49 @@
+const carro=document.getElementById("carro") //puxando elementos DOM
+const btn_esquerda=document.getElementById("btn_esquerda")
+const btn_direita=document.getElementById("btn_direita")
+
+const init=()=>{
+    carro.style="position:relative;left:0px" //Inicia com style no id carro
+}
+
+btn_esquerda.addEventListener("click",()=>{
+       
+    carro.style.left=parseInt(carro.style.left) - 10 +"px"
+})
+
+btn_direita.addEventListener("click",()=>{
+    let pos=parseInt(carro.style.left) //converte o valor do left para inteiro
+    pos+=10
+    carro.style=`position:relative;left:${pos}px`
+})
+
+window.onload=init //Ao carregar a pagina chama a função
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
