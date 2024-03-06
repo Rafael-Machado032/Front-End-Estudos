6c837e9e64addade0f830f123eabdf72dd9820aa
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2024-03-05 23:49:34 -0300

    atualiza

---
+++ JavaScript Estudos/Projeto JavaScript 4 Animação Automatico/script.js
@@ -0,0 +1,58 @@
+const carro=document.getElementById("carro") //puxando elementos DOM
+const btn_parar=document.getElementById("btn_parar")
+const btn_esquerda=document.getElementById("btn_esquerda")
+const btn_direita=document.getElementById("btn_direita")
+
+const init=()=>{
+    carro.style="position:relative;left:0px" //Inicia com style no id carro
+}
+
+let anima=null
+
+const move=(dir)=>{ //É chamada a função e recebe o argumento de 1 ou -1
+    carro.style.left=parseInt(carro.style.left) + (10*dir) +"px" //Incremento ou decremento do left de 10px vezes 1 ou -1
+}
+btn_parar.addEventListener("click",()=>{
+    clearInterval(anima) //Para o interval
+})
+
+btn_esquerda.addEventListener("click",()=>{
+    clearInterval(anima) //para o interval e cria um novo intervalo na proxima linha
+    anima=setInterval(move,100,-1) //Chama a função move a cada 100 milissegundos e o argumento 1 que passa para o dir
+})
+
+btn_direita.addEventListener("click",()=>{
+    clearInterval(anima)
+    anima=setInterval(move,100,1) //Chama a função move a cada 100 milissegundos e o argumento -1 que passa para o dir
+})
+
+window.onload=init //Ao carregar a pagina chama a função
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JavaScript Estudos/Projeto JavaScript 4 Animação/script.js
+++ JavaScript Estudos/Projeto JavaScript 4 Animação/script.js
@@ -8,12 +8,12 @@ const init=()=>{
 
 btn_esquerda.addEventListener("click",()=>{
        
-    carro.style.left=parseInt(carro.style.left) - 10 +"px"
+    carro.style.left=parseInt(carro.style.left) - 10 +"px" //Decremento do left para -10px
 })
 
 btn_direita.addEventListener("click",()=>{
     let pos=parseInt(carro.style.left) //converte o valor do left para inteiro
-    pos+=10
+    pos+=10 //Incrementa o left para 10px
     carro.style=`position:relative;left:${pos}px`
 })
 
