59510fb3e41889de909cd34493b3a63793a5c987
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-03-08 23:48:30 -0300

    atualiza

---
+++ JavaScript Estudos/Projeto JavaScript 5 accesKey/script.js
@@ -0,0 +1,48 @@
+const q1 = document.getElementById("q1")
+const q2 = document.getElementById("q2")
+const posx = document.getElementById("posx")
+const posy = document.getElementById("posy")
+const largura = document.getElementById("largura")
+const altura = document.getElementById("altura")
+
+q1.accessKey="b" //Tecla de atalho Alt "b"
+q2.accessKey="n" //Tecla de atalho Alt "n"
+
+const info = (el) => {
+    let dados = el.getBoundingClientRect() //Retorna posição e tamanho dos elementos
+    posx.innerHTML = `posx: ${dados.x}`
+    posy.innerHTML = `posy: ${dados.y}`
+    altura.innerHTML = `largura: ${dados.width}`
+    largura.innerHTML = `altura: ${dados.height}`
+}
+
+
+q1.addEventListener("click", (evt) => {
+    info(evt.target) //retorna o elemento onde ocorreu o evento 
+})
+q2.addEventListener("click", (evt) => {
+    info(evt.target)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
