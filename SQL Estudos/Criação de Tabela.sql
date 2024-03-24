849da7e2288ac02d47bcdaf76e365c3eab66940a
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-03-24 16:28:18 -0300

    a

--- SQL Estudos/Criação de Tabela.sql
+++ SQL Estudos/Criação de Tabela.sql
@@ -52,3 +52,5 @@ COLLATE = utf8mb4_0900_ai_ci;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+--Salvamento criado pelo MySQL através do relacionamento de diagrama
--- SQL Estudos/chave estrangeira.sql
+++ SQL Estudos/chave estrangeira.sql
@@ -1,13 +1,16 @@
+--Alteração do tipo do campo
 alter table cliente modify column i_idade_cliente tinyint;
 alter table cliente add i_idade_cliente int;
 alter table cliente modify column i_cliente_cliente int;
 
-
+--criado a tabela tipo_cliente
 create table tipo_cliente( 
     i_tipocliente_tipocliente INT PRIMARY KEY AUTO_INCREMENT,
     s_dectipocliente_tipocliente VARCHAR(100) NOT NULL
 );
 
+--Fazendo a ligação da chave primaria com a estrangeira
 ALTER TABLE cliente ADD CONSTRAINT fk_tipocliente FOREIGN KEY (i_tipo_cliente) REFERENCES tipo_cliente (i_tipocliente_tipocliente);
 
+--Inserindo coluna
 alter table cliente add i_tipo_cliente int;
