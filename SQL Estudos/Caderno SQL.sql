237a970e98c781d6cc8e77352c4f77c8d54b2486
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-03-30 22:46:57 -0300

    a

--- SQL Estudos/Caderno SQL.sql
+++ SQL Estudos/Caderno SQL.sql
@@ -1,7 +1,19 @@
 --MySQL
 --DDL Data Definition Language (Linguagem de Definição de dados)
-/*Criar Banco de Dados*/
-/*Para criar um banco de dados seleciona "Query/New Tab to Current Serve"*/
+/*Comentários*/
+SELECT * FROM cliente;
+#Comentário de uma linha
+SELECT * FROM venda;
+--Comentário de uma linha
+SELECT * FROM produtovenda;
+/*Comentário de 
+de mais linha*/
+SELECT * FROM tipo_cliente;
+
+-------------------------------------------------------------------------------------------------------------
+/*Criar Banco de Dados
+Para criar um banco de dados seleciona "Query/New Tab to Current Serve"
+*/
 CREATE SCHEMA estudoSQL;
 /*ou*/
 CREATE DATABASE estudoSQL;
@@ -409,16 +421,15 @@ SELECT MAX(f_valor_venda) FROM venda;
 SELECT MIN(f_valor_venda) FROM venda;
 -- Valor Mínimo
 
-SELECT 
-    c.s_nome_cliente,
-    v.i_venda_venda,
-    v.d_data_venda,
-    v.f_valor_venda
- FROM venda v
+SELECT c.s_nome_cliente, v.i_venda_venda, v.d_data_venda, v.f_valor_venda
+FROM venda v
     INNER JOIN cliente c ON c.i_cliente_cliente = v.i_cliente_cliente
-    WHERE f_valor_venda = (SELECT MAX(f_valor_venda) FROM venda);
-    -- Retorna os dados da maior venda
-
+WHERE
+    f_valor_venda = (
+        SELECT MAX(f_valor_venda)
+        FROM venda
+    );
+-- Retorna os dados da maior venda
 -------------------------------------------------------------------------------------------------------------
 /*Funções
 SUM   -> Soma       -> SUM(COLUNA_NUM)
@@ -426,23 +437,29 @@ AVG   -> Media      -> AVG(COLUNA_NUM)
 COUNT -> Contagem   -> COUNT(COLUNA_NUM)
 ROUND -> Arredondar -> ROUND(COLUNA_NUM,CASAS_DECIMAIS)
 */
+SELECT ROUND(SUM(f_valor_venda), 2)
+FROM venda
+WHERE
+    d_data_venda > '2020-05-01';
 
-SELECT ROUND(SUM(f_valor_venda),2) FROM venda
-WHERE d_data_venda > '2020-05-01';
+SELECT ROUND(AVG(f_valor_venda), 2) FROM venda;
 
-SELECT ROUND(AVG(f_valor_venda),2) FROM venda;
-
-SELECT 
-    COUNT(f_valor_venda) AS 'Quantidade vendas',
-    ROUND(AVG(f_valor_venda),2) AS 'Valor médio das vendas' FROM venda
-WHERE 
-    f_valor_venda > (SELECT MIN(f_valor_venda) FROM venda) AND
-    f_valor_venda < (SELECT MAX(f_valor_venda) FROM venda);
+SELECT COUNT(f_valor_venda) AS 'Quantidade vendas', ROUND(AVG(f_valor_venda), 2) AS 'Valor médio das vendas'
+FROM venda
+WHERE
+    f_valor_venda > (
+        SELECT MIN(f_valor_venda)
+        FROM venda
+    )
+    AND f_valor_venda < (
+        SELECT MAX(f_valor_venda)
+        FROM venda
+    );
 -- Calcula media tirando o valor máximo e mínimo das vendas
 
 -------------------------------------------------------------------------------------------------------------
-/*Operador LIKE
 
+/*Operador LIKE
 Caracteres coringa
 % -> Qual quer carácter
 _ -> Quantidade de carácter
@@ -453,96 +470,295 @@ LIKE('r%l') -> Começa com "R" e termina com a letra "L"
 LIKE('r_____') -> Começa com "R" mais 5 caráter pois tem 5 underline
 LIKE('______') -> 6 caráter pois te 6 underline
 */
-SELECT * FROM cliente
-WHERE s_nome_cliente LIKE('v%o');
+SELECT * FROM cliente WHERE s_nome_cliente LIKE('v%o');
 
-SELECT * FROM cliente
-WHERE s_nome_cliente LIKE('rafa_________s.');
+SELECT * FROM cliente WHERE s_nome_cliente LIKE('rafa_________s.');
 
 -------------------------------------------------------------------------------------------------------------
 /*Operador IN
 Quando usar muito OR na filtragem, para não ficar muito grande utilizamos IN e reduzimos esse comando
 A função RIGHT(STRING,QUANTIDADE) extrai vários caracteres de uma string (começando da direita)
 */
-SELECT * FROM cliente
-WHERE RIGHT(s_cpf_cliente,3) IN (011,033);
+SELECT * FROM cliente WHERE RIGHT(s_cpf_cliente, 3) IN (011, 033);
 -- Retorna os dados dos clientes que o cpf termina com esses valores
 
-SELECT * FROM cliente
-WHERE d_nasc_cliente IN (
-    SELECT d_nasc_cliente FROM cliente
-    WHERE
-        d_nasc_cliente > '2000-01-01' AND
-        d_nasc_cliente < '2010-10-01'
-); -- SubSelect que retorna a data dentro dessa condição
+SELECT *
+FROM cliente
+WHERE
+    d_nasc_cliente IN (
+        SELECT d_nasc_cliente
+        FROM cliente
+        WHERE
+            d_nasc_cliente > '2000-01-01'
+            AND d_nasc_cliente < '2010-10-01'
+    );
+-- SubSelect que retorna a data dentro dessa condição
 -- Retorna os dados dos clientes que atende a condição do SubSelect
-
 -------------------------------------------------------------------------------------------------------------
 /*BETWEEN
 Filtra entre valores máximo e mínimo
 */
-SELECT * FROM cliente
-WHERE d_nasc_cliente BETWEEN '2000-01-01' and '2010-01-01';
-
-SELECT * FROM venda;
+SELECT *
+FROM cliente
 WHERE
-    (d_data_venda BETWEEN '2020-07-01' AND '2020-11-01') AND
-    i_cliente_cliente NOT IN (3,10) AND
-    f_valor_venda > 400;
+    d_nasc_cliente BETWEEN '2000-01-01' and '2010-01-01';
 
--------------------------------------------------------------------------------------------------------------   
+SELECT *
+FROM venda
+WHERE (
+        d_data_venda BETWEEN '2020-07-01' AND '2020-11-01'
+    )
+    AND i_cliente_cliente NOT IN(3, 10)
+    AND f_valor_venda > 400;
+
+-------------------------------------------------------------------------------------------------------------
+
 /*Tipos de JOIN (Junção de 2 tabelas)
-
 INNER JOIN      ->  Comum entre duas tabelas
 LEFT JOIN       ->  Toda a tabela1 mais o comum da tabela2
 RIGHT JOIN      ->  Toda a tabela2 mais o comum da tabela1
 FULL OUTER JOIN ->  Toda a tabela1 e tabela2
 */
 
-SELECT i_cliente_cliente FROM cliente c;
+SELECT c.i_cliente_cliente, c.s_nome_cliente, v.d_data_venda, v.f_valor_venda
+FROM cliente c
+    INNER JOIN venda v ON c.i_cliente_cliente = v.i_cliente_cliente;
+-- Retorna o comum entre as 2 tabelas
+
+SELECT c.i_cliente_cliente, c.s_nome_cliente, v.d_data_venda, v.f_valor_venda
+FROM cliente c
+    LEFT JOIN venda v ON c.i_cliente_cliente = v.i_cliente_cliente;
+-- Prioriza a tabela cliente ou seja aparece cliente que nao teve venda
+
+SELECT c.i_cliente_cliente, c.s_nome_cliente, v.d_data_venda, v.f_valor_venda
+FROM cliente c
+    RIGHT JOIN venda v ON c.i_cliente_cliente = v.i_cliente_cliente;
+-- Prioriza a tabela venda, aparece somente cliente que fez venda
+
+-------------------------------------------------------------------------------------------------------------
+/*self JOIN
+Faz correlação com a mesma tabela*/
+
+SELECT *
+FROM cliente c1, cliente c2
+WHERE
+    c1.i_tipo_cliente = c2.i_tipo_cliente
+    AND -- Todos os cliente que tem o mesmo tipo
+    c1.s_nome_cliente = "Rafael M. F. S." -- Correlaciona com o cliente
+ORDER BY c1.i_tipo_cliente;
+
+-------------------------------------------------------------------------------------------------------------
+/*UNION
+Une a tabela um baixo do outro*/
+
+SELECT
+    'Cliente' as Tabela,
+    i_cliente_cliente,
+    s_nome_cliente
+FROM cliente
+UNION
+SELECT
+    'Cliente Backup',
+    i_cliente_cliente,
+    s_nome_cliente
+FROM cliente_backup
+UNION
+SELECT 'Venda', v.i_cliente_cliente, c.s_nome_cliente
+FROM venda v
+    INNER JOIN cliente c ON v.i_cliente_cliente = c.i_cliente_cliente;
+
+-------------------------------------------------------------------------------------------------------------
+/*EXISTS
+É usado para verificar se tem registro da sub-consulta
+O resultado é True ou False*/
+
+SELECT *
+FROM cliente c -- So retorna os clientes que fizeram venda
+WHERE
+    EXISTS (
+        SELECT *
+        FROM venda v
+        WHERE
+            v.i_cliente_cliente = c.i_cliente_cliente -- Faz o correlação
+    );
+
+-------------------------------------------------------------------------------------------------------------
+
+/*CASE WHEN
+Retorna quando uma das condição é atendida
+CASE 
+WHEN Condição1  THEN  Resultado1
+WHEN Condição2  THEN  Resultado2
+WHEN Condição3  THEN  Resultado3
+ELSE Resultado sem atender condição 
+END
+DATEDIFF(data1, data2) -> Diferença de duas data e retorna em dias
+*/
+SELECT
+    i_cliente_cliente,
+    s_nome_cliente,
+    s_cpf_cliente,
+    CASE
+        WHEN d_nasc_cliente is NULL THEN NOW()
+        ELSE d_nasc_cliente
+    END 'Data de nascimento',
+    i_tipo_cliente,
+    CASE
+        WHEN (
+            DATEDIFF(NOW(), d_nasc_cliente) / 365
+        ) > 18 THEN 'Maior'
+        ELSE 'Menor'
+    END 'Situação'
+FROM cliente;
+
+-------------------------------------------------------------------------------------------------------------
+
+/* IFNULL, ISNULL e NVL
+MySQL     -> IFNULL ou COALESCE
+SqlServer -> ISNULL
+Oracle    -> NVL
+IFNULL(coluna,valor) -> CAso for nulo retorna o valor se não retorna coluna
+*/
+
+#UPDATE produtovenda SET i_qtde_produtovenda=NULL WHERE i_produtovenda_produtovenda=1;
+SELECT *, ROUND(
+        (
+            f_precoun_produtovenda * IFNULL(i_qtde_produtovenda, 0)
+        ), 2
+    ) AS 'Total'
+FROM produtovenda;
+
+-------------------------------------------------------------------------------------------------------------
+
+/*STORED PROCEDURE
+Procedimento armazenado
+É uma consulta avançado que podemos criar e chamar a qualquer momento
+#DELIMITER $$
+#CREATE PROCEDURE Função(IN <Parâmetro de entrada>, OUT <Parâmetro de saída>)
+#BEGIN
+#   Comandos de programação
+#END $$
+#DELIMITER ;
+CALL Entrada(Parâmetro)
+*/
+
+DELIMITER $$ -- Mudamos o delimitador de ";" para "$$" para não confundir com delimitador padrão do MySQL
+
+CREATE PROCEDURE curso(
+
+IN aluno VARCHAR(50)) -- IN indicando parâmetro de entrada, nome da variável e o tipo
+BEGIN
+    DECLARE x VARCHAR(50);
+--Declara uma variável "x"
+    SET x = 'MySQL';
+-- Seta o conteúdo na variável "x"
+    SELECT x, aluno;
+-- Mostra o conteúdo de "x" e "curso"
+END $$ -- finaliza
+
+DELIMITER; -- Volta o delimitador para o padrão
+
+CALL curso ('Rafael Machado');
+-- chama a função
 
 
 
+DELIMITER $$
+
+CREATE PROCEDURE idade(
+    IN idCliente INT, 
+    OUT idade INT, 
+    OUT res VARCHAR(20)
+) 
+BEGIN 
+    DECLARE dt DATETIME;
+    SET dt = (
+	    SELECT d_nasc_cliente
+	    FROM cliente
+	    WHERE i_cliente_cliente = idCliente
+    );
+
+    SET idade = YEAR(NOW()) - YEAR(dt);
+
+    IF(idade >= 18) THEN 
+        SET res = 'Maior';
+    ELSE 
+        SET res = 'Menor';
+    END	IF;
+END $$ 
+DELIMITER ;
+
+CALL idade (3, @idadecliente,@resultado);
+-- Variável idadecliente
+SELECT @idadecliente,@resultado;
+
+-------------------------------------------------------------------------------------------------------------
+/*LOOP
+Estrutura de repetição*/
+
+DELIMITER $$
+CREATE PROCEDURE loopWhile(IN max INT, OUT soma INT)
+BEGIN
+    DECLARE x INT DEFAULT 0;
+
+    WHILE(x < max)DO
+        SET x = x + 1;
+    END WHILE;
+
+    SET soma = x;
+END $$
+DELIMITER;
+
+CALL loopWhile(100,@ret);
+SELECT @ret;
 
 
 
+DELIMITER $$
+CREATE PROCEDURE loopWhile2(IN max INT, OUT soma INT)
+BEGIN
+    DECLARE x INT DEFAULT 0;
+    meuloop:LOOP
+        IF(x >= max)THEN
+            LEAVE meuloop; -- Sai do loop
+        END IF;
+        SET x = x + 1;
+    END LOOP;
+    SET soma = x;
+END $$
+DELIMITER;
 
+CALL `loopWhile2`(100,@ret);
+SELECT @ret;
 
+-------------------------------------------------------------------------------------------------------------
+/*CURSOR
+Navegar em entre elementos de uma consulta*/
 
+DELIMITER $$
+CREATE PROCEDURE somaVendas(OUT soma FLOAT(10,2))
+BEGIN
+    DECLARE vv FLOAT(10,2) DEFAULT 0;
+    DECLARE fimloop INT DEFAULT 0;
+    DECLARE meucursor CURSOR FOR SELECT f_valor_venda FROM venda; -- Declara o cursor com um SELECT a onde vai percorrer
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET fimloop=1; -- Quando terminar a lista da tabela o fimloop vai ser igual a 1
+    
+    SET soma=0;
+    OPEN meucursor; -- Abre o CURSOR para começar
+    
+    WHILE(fimloop != 1)DO
+        FETCH meucursor INTO vv; -- Coloca no vv e busca a proxima linha
+        SET soma = soma + vv; -- Soma os valores
+    END WHILE;
 
+END $$
+DELIMITER;
 
+CALL somaVendas(@ret);
+SELECT ROUND(@ret,2);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-------------------------------------------------------------------------------------------------------------
+/**/
 
 
 
