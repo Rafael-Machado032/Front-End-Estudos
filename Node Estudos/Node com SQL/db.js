04d0df3aa05a0563c6027c09f7408f2adaf76157
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2024-04-03 00:07:08 -0300

    erro no cod

--- Node Estudos/Node com SQL/db.js
+++ Node Estudos/Node com SQL/db.js
@@ -16,7 +16,7 @@ const todosClientes = async () => {
     return await linhas
 }
 
-//Inserir na tabela
+//Inserir na tabela INSERT INTO
 const insereClientes = async (cliente) => {
     const con = await conectar()
     const sql = 'INSERT INTO clientenode(s_nome_clientenode,s_idade_clientenode) VALUES (?,?)'
@@ -25,4 +25,22 @@ const insereClientes = async (cliente) => {
 }//O "valores" recebe o nome e idade e bota no "?" na query
 
 
-module.exports = { todosClientes,insereClientes }
+//Inserir na tabela UPDATE
+const atualizaClientes = async (id,cliente) => {
+    const con = await conectar()
+    const sql = 'UPDATE clientenode SET s_nome_clientenode = ?, s_idade_clientenode = ? WHERE i_clientenode_clientenode = ?;'
+    const valores = [cliente.nome,cliente.idade,id]
+    await con.query(sql,valores)
+}
+
+//Inserir na tabela UPDATE
+const deletarClientes = async (id) => {
+    const con = await conectar()
+    const sql = 'DELETE FROM clientenode  WHERE i_clientenode_clientenode = ?;'
+    const valores = [id]
+    await con.query(sql,valores)
+}
+
+
+
+module.exports = { todosClientes,insereClientes,atualizaClientes,deletarClientes}
