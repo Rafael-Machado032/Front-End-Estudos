11c29c60887525f66121ddcb9e7be79f90715cf3
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2024-04-18 22:21:53 -0300

    a

---
+++ Node Estudos/NodeRed/inicio.js
@@ -0,0 +1,21 @@
+//Verificar versão do node 
+//Usando Powershell: node --version; npm --version
+//Usando cmd: node --version && npm --version
+
+//Resultado:
+// v20.11.1
+// 10.2.4
+
+//Instalando NodeRed no CMD:
+//C:\Users\rafin>npm install -g --unsafe-perm node-red
+
+//E para executar
+//C:\Users\rafin>node-red
+
+//Para sair aperte "Ctrl + c"
+
+//Por padrão ele roda na porta 1880 
+//(http://127.0.0.1:1880/) mas para mudar entre com o comando:
+//C:\Users\rafin>node-red -p 8080
+//(http://127.0.0.1:8080/)
+//Esse endereço localHost abre a plataforma para programação
