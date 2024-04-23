b3d96c1c20a87b534ba2f004d1da55f17e0569fb
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-04-22 22:32:39 -0300

    a

--- Node Estudos/NodeRed/inicio.js
+++ Node Estudos/NodeRed/inicio.js
@@ -19,3 +19,33 @@
 //C:\Users\rafin>node-red -p 8080
 //(http://127.0.0.1:8080/)
 //Esse endereço localHost abre a plataforma para programação
+
+//*Tipos de nós
+
+//Inject    -> Nó de incio: Injeta uma mensagem porem não necessariamente precisa começar com ele.
+//          Para alterar os parâmetros da injeção de um click dublo
+//          Repetição configurada
+
+//Debug     -> Nó de saída: Recebe o que foi injetado como se fosse um console
+//          Escolhe um ou todos os recursos injetado
+
+//Function  -> Nó de função: Pode fazer função em javascript
+//          No inicio:
+//          O código adicionado aqui será executado uma vez
+//          sempre que o nó for iniciado.
+//
+//          Na mensagem:
+//          Retorna uma mensagem
+//
+//          Na parada:
+//          O código adicionado aqui será executado quando o nó
+//          for interrompido ou reimplementado.
+//
+//          1 ou mais saídas e 1 ou mais entradas
+//          
+
+
+
+
+
+
