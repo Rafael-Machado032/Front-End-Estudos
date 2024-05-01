c833a9e7c9d75ecaa4e094a38ed7d0265aa0dd76
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2024-05-01 20:41:10 -0300

    a

--- Node Estudos/NodeRed/Caderno NodeRED.js
+++ Node Estudos/NodeRed/Caderno NodeRED.js
@@ -22,13 +22,14 @@
 
 //*Tipos de nós
 
+//#Comum
 //Inject        -> Nó de incio: Injeta uma mensagem porem não necessariamente precisa começar com ele.
 //              Para alterar os parâmetros da injeção de um click dublo
 //              Repetição configurada
-
 //Debug         -> Nó de saída: Recebe o que foi injetado como se fosse um console
 //              Escolhe um ou todos os recursos injetado
 
+//#Função
 //Function      -> Nó de função: Pode fazer função em javascript
 //              No inicio:
 //              O código adicionado aqui será executado uma vez
@@ -42,14 +43,10 @@
 //              for interrompido ou reimplementado.
 //
 //              1 ou mais saídas e 1 ou mais entradas
-
 //Switch        -> Nó de switch: Serve para dependendo da condição,
 //              sair em um sada especifica
-
 //Change        -> Nó de modificação: Serve para definir, mudar, mover ou apagar valores programados
-
 //Template      -> Nó de apresentação: Insere um texto junto com o resultado
-
 //Range         -> Nó de mapeamento: Correlaciona valores entre 2 faixas 
 //              ex:
 //              Mapeie o intervalo de entrada:
@@ -66,12 +63,11 @@
 //                          x = 40
 //              Outro exemplo é uma saída de um sensor de 0 a 12v
 //              sendo 0v e igual -10° e 12v é igual 40° e ao receber 6v seria igual á 25°
-
 //Delay         -> Nó de intervalo: Atrasa o evento por tempo programado
-
 //Trigger       -> Nó de Gatilho: Ao ativar o gatilho fica disparando mensagem programada
 //              podemos parar de dispara ao receber um valor especifico
 
+//#Rede
 //HTTP          -> Nó de web: Pode fazer chamadas via browser como API
 //              EX: Na configuração do no HTTP setamos "aluno" no URL
 //              http://127.0.0.1:1880/aluno?curso=Javascript Nesse exemplo passamos um parâmetro
@@ -82,19 +78,40 @@
 //              Mais parâmetros
 //              "/api/:nome/:idade/:profissão"
 //              http://127.0.0.1:1880/api/Rafael/34/Tecnico
-
-//File          -> No de armazenamento: Permite selecionar um arquivo para leitura
-
 //HTTP Request  -> Nó de requisição http: Permite consultar url configurada nela
 
-//HTML          -> Nó de seleção: Seleciona qualquer elemento dentro do HTML
-
+//#Sequencia
 //Split         -> Nó de divisão: Divide a mensagem a partir do carácter definido e cria um array
-
 //Join          -> Nó de junção: Junta as mensagem enviadas separadas setando no manual e 
 //              a quantidade de mensagem
 
-//mysql         -> Nó de Banco de Dados: As query é inserido no msg.topic
+//#Analisador sintático
+//HTML          -> Nó de seleção: Seleciona qualquer elemento dentro do HTML
+
+//#Armazenamento
+//File          -> No de armazenamento: Permite selecionar um arquivo para leitura
+//mysql         -> Nó de Banco de Dados: As query é inserido no msg.topic e sai msg.payload
+
+//#Dashboard
+//                  Aba do dashboard
+//
+//  Layout: Organização de elementos do dashboard
+//      tab: Pagina onde colocar elementos (Adiciona no menu sandwich)
+//          group: Onde associamos os elementos
+//          Todos os elementos são obrigados a associar no group
+//  Site: Configuração geral da pagina do dashboard
+//  Theme: Configuração visual da pagina do dashboard
+//
+//  Endereço do dashboard http://127.0.0.1:1880/ui/
+
+//Button        -> Nó de botão: Manda a carga pra quem estiver o botão associado
+//              A mensagem manda para msg.payload
+//Text          -> Nó de texto de exibição: Exibe um texto que for mandado pra ele 
+//Text Input    -> Nó de texto de envio: Envia o texto inserido nele
+//              No delay vem configurado 300ms para envio auto mas se zeramos fica manual ou seja 
+//              sera necessário um elemento para disparo
+//Gauge         -> Nó gráfico de giro: Existe 4 tipos de gráficos gauge, donut, compass, level
+//Slider        -> Nó de slide: Uma barra com uma bolinha de arrastar            
 
 
 //*Funções Prontos
