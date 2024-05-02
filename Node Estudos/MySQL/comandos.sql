a4c309095c02e319791bab216fa44a6363353285
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-05-01 23:11:11 -0300

    a

--- Node Estudos/MySQL/comandos.sql
+++ Node Estudos/MySQL/comandos.sql
@@ -15,3 +15,10 @@ SELECT * FROM clientenode;
 
 ALTER TABLE clientenode
 ADD i_clientenode_clientenode INT PRIMARY KEY NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE contatos_nodeRED (
+    i_contatos_contatos INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    s_nome_contatos VARCHAR(45), 
+    s_telefone_contatos VARCHAR(45),
+    s_email_contatos VARCHAR(45)
+);
