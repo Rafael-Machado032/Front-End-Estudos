76ee68e50744600cb46f16e5086a2d47f0361d11
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2024-06-06 23:15:58 -0300

    aaa

--- Node Estudos/NodeRed/Front-End com NodeRed/script/consulta.js
+++ Node Estudos/NodeRed/Front-End com NodeRed/script/consulta.js
@@ -1,6 +1,12 @@
-const obterRegistro = async() =>{
-    const res = await fetch ('http://127.0.0.1:1880/consultarB')
+const obterRegistro = async () => {
+    const res = await fetch('http://127.0.0.1:1880/consultarB')
     const resJson = await res.json()
-    document.getElementById("dados").innerHTML=JSON.stringify(resJson)
+
+    resJson.map(
+        (dado) => {
+            document.getElementById("dados").innerHTML += "<div>ID: " + dado.i_contatos_contatos + " - NOME: " + dado.s_nome_contatos + " - TELEFONE: " + dado.s_telefone_contatos + " - EMAIL: " + dado.s_email_contatos + "</div>";
+        }
+    )
 }
+
 obterRegistro();
