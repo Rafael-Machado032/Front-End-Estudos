8a7c17205287e911d6627a68c6a0e1bd8b0789a0
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-06-06 23:15:58 -0300

    aaa

--- Node Estudos/NodeRed/Front-End com NodeRed/script/adicionar.js
+++
@@ -1,6 +0,0 @@
-const adicionarRegistro = async() =>{
-    const res = await fetch ('http://127.0.0.1:1880//adicionarB')
-    const resJson = await res.json()
-    document.getElementById("dados").innerHTML=JSON.stringify(resJson)
-}
-adicionarRegistro();
