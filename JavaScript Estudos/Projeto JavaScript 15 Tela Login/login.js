b2c571b9cc327cbc75176a52bd4fe0cf5ced5824
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-07-22 22:40:55 -0300

    a

--- JavaScript Estudos/Projeto JavaScript 15 Tela Login/login.js
+++ JavaScript Estudos/Projeto JavaScript 15 Tela Login/login.js
@@ -5,12 +5,19 @@ class Login {
     static nomelogado = null;
     static acessologado = null;
     static estilocss = null;
+    static config = {
+        cor: "#048",
+        img: "./logo.jpg"
+    };
     static endpoint = "https://ea5e330f-3e67-46f7-96a3-4e29209807d2-00-1rhphb7s2ac4p.riker.replit.dev/"
 
-    static login = (mat, pas) => {
+    static login = ( mat, pas, config = null ) => {
+        if (config!=null) {
+            this.config = config;
+        }
+        
         this.endpoint += `?matricula=${mat}&senha=${pas}`;
         this.estilocss =
-            " * { border: none; padding: 0px; margin: 0px; box- sizing: border - box; }"+
             ".fundologin{ display: flex; justify-content: center; align-items: center; width: 100%; height: 100vh; position: absolute; top: 0px; left: 0px; background-color: rgba(0, 0, 0, 0.75); box-sizing: border-box; }" +
             ".baselogin{ display: flex; justify-content: center; align-items: stretch;width: 50%; box-sizing: inherit; }" +
             ".elementoslogin{ display: flex;justify-content: center;align-items: flex-start; flex-direction: column; width: 50%; background-color: #eee; padding: 10px; border-radius: 10px 0px 0px 10px; box-sizing: inherit; }" +
@@ -20,7 +27,7 @@ class Login {
             ".campologin label{ font-size: 18px; }"+
             ".campologin input{ font-size: 18px; padding: 5px;background-color: #fff;border-radius: 5px;}" +
             ".botoeslogin{ display: flex;justify-content: space-around; align-items: center; width: 100%;box-sizing: inherit; }" +
-            ".botoeslogin button{ cursor: pointer; background-color: #048;color: #fff; border-radius: 5px; padding: 10px; width: 100px; box-sizing: inherit; }"
+            `.botoeslogin button{ cursor: pointer; background-color: ${this.config}; color: #fff; border-radius: 5px; padding: 10px; width: 100px; box-sizing: inherit; }`
         
             const styleestilo = document.createElement("style");
             styleestilo.setAttribute("id","id_estilologin");
@@ -28,7 +35,79 @@ class Login {
             styleestilo.setAttribute("type","text/css");
             styleestilo.innerHTML = this.estilocss;
             document.head.appendChild(styleestilo);
+
+            const corpo = document.body;
+
+            const fundologin = document.createElement("div");
+            fundologin.setAttribute("id","fundologin");
+            fundologin.setAttribute("class","fundologin");
+            document.body.prepend(fundologin);
+
+            const baselogin = document.createElement("div");
+            baselogin.setAttribute("id","baselogin");
+            baselogin.setAttribute("class","baselogin");
+            fundologin.appendChild(baselogin);
+            
+            const elementoslogin = document.createElement("div");
+            elementoslogin.setAttribute("id","elementoslogin");
+            elementoslogin.setAttribute("class","elementoslogin");
+            baselogin.appendChild(elementoslogin);
+
+            const campologinusername = document.createElement("div");
+            campologinusername.setAttribute("id","campologinusername");
+            campologinusername.setAttribute("class","campologin");
+            elementoslogin.appendChild(campologinusername);
+
+            const labelusername = document.createElement("label");
+            labelusername.innerHTML = "User Name:"
+            campologinusername.appendChild(labelusername);
+
+            const inputusername = document.createElement("input");
+            inputusername.setAttribute("id","f_username");
+            inputusername.setAttribute("type","text");
+            inputusername.setAttribute("name","f_username");
+            campologinusername.appendChild(inputusername);
+
+            const campologinsenha = document.createElement("div");
+            campologinsenha.setAttribute("id","campologinsenha");
+            campologinsenha.setAttribute("class","campologin");
+            elementoslogin.appendChild(campologinsenha);
+
+            const labelsenha = document.createElement("label");
+            labelsenha.innerHTML = "Senha:"
+            campologinsenha.appendChild(labelsenha);
+
+            const inputsenha = document.createElement("input");
+            inputsenha.setAttribute("id","f_senha");
+            inputsenha.setAttribute("type","password");
+            inputsenha.setAttribute("name","f_senha");
+            campologinsenha.appendChild(inputsenha);
+
+            const botoeslogin = document.createElement("div");
+            botoeslogin.setAttribute("id","botoeslogin");
+            elementoslogin.appendChild(botoeslogin);
+
+            const btn_login = document.createElement("button");
+            btn_login.setAttribute("id","btn_login");
+            btn_login.innerHTML = "Login"
+            elementoslogin.appendChild(btn_login);
            
+            const btn_cancelar = document.createElement("button");
+            btn_cancelar.setAttribute("id","btn_login");
+            btn_cancelar.innerHTML = "Cancelar"
+            elementoslogin.appendChild(btn_cancelar);
+
+            const logologin = document.createElement("div");
+            logologin.setAttribute("id","logologin");
+            logologin.setAttribute("class","logologin");
+            baselogin.appendChild(logologin);
+           
+            const img_logologin = document.createElement("img");
+            img_logologin.setAttribute("src",this.config.img);
+            img_logologin.setAttribute("title","Tela Login");
+            logologin.appendChild(img_logologin);
+           
+            
         //     fetch(this.endpoint)
         //     .then(res => res.json())
         //     .then(res => {
