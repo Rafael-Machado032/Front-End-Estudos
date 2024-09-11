b0512744379c9b9e1b01acf77681ae2c78584ebb
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-09-10 23:20:46 -0300

    a

--- Sistema de Manutenção/script/script.js
+++ Sistema de Manutenção/script/script.js
@@ -1,26 +1,40 @@
 import Rebot from "../layout/rebot.js";
 import { Config } from "../layout/config.js";
-
+import { AddJanela } from "../script/addjanela.js";
 
 Rebot.button_rebot();
 Rebot.janela_rebot();
 Config.button_config();
-//
 
-const ocultar = () =>{
-    const selecionar = document.getElementById("container_dir_painel");
-    selecionar.remove();
-}
+//
 const menu1 = document.getElementById("reiniciar");
+const menu2 = document.getElementById("configuracao");
+menu1.classList.add("selecionado");
+
+
+const ocultar = () => {
+    const excluircont = document.getElementById("container_dir_painel");
+    excluircont.remove();
+    menu1.classList.remove("selecionado");
+    menu2.classList.remove("selecionado");
+}
+
 
 menu1.addEventListener("click", () => {
     ocultar();
     Rebot.janela_rebot();
+    menu1.classList.add("selecionado");
 })
 
-const menu2 = document.getElementById("configuracao");
 
 menu2.addEventListener("click", () => {
     ocultar();
     Config.painel_config();
+    menu2.classList.add("selecionado");
+})
+
+const bnovo_dispositivo1 = document.getElementById("bnovo_dispositivo");
+
+bnovo_dispositivo1.addEventListener("click", () => {
+    AddJanela.addjanela();
 })
