484b434f54a24515082964e40a0ca12bbe1396b2
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2024-10-04 00:11:23 -0300

    a

---
+++ Sistema de Manutenção/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config(); // Carregar as variáveis de ambiente
+//Essa linha carrega as variáveis de ambiente do arquivo .env. O pacote dotenv permite que você defina variáveis sensíveis (como senhas ou strings de conexão) em um arquivo separado, para que não fiquem expostas no código-fonte. Quando você chama config(), todas as variáveis definidas no .env ficam disponíveis em process.env.
+
+const mongoose = require('mongoose');
+//Aqui, você está importando o módulo Mongoose, que é uma biblioteca para facilitar a interação com o MongoDB usando esquemas e modelos.
+const express = require('express');
+//Essa linha importa o módulo Express, um framework web para Node.js, que facilita a criação de aplicações web e APIs.
+const app = express();
+//Aqui, você cria uma instância do aplicativo Express. Essa instância (app) será usada para definir rotas e middleware.
+const port = process.env.PORT || 3000;
+//Essa linha define a porta em que o servidor irá escutar. Ele primeiro tenta pegar a variável de ambiente PORT (definida no .env) e, se não existir, usa a porta 3000 como padrão
+
+// Atualizar string de conexão do MongoDB
+const uri = process.env.MONGO_URI; // Agora usamos a variável de ambiente
+//Aqui, você obtém a string de conexão do MongoDB, que foi definida no .env. Isso permite que você se conecte ao banco de dados usando a string correta, sem expor informações sensíveis no código.
+
+// Conectar ao MongoDB com Mongoose
+async function connectMongoDB() { //Essa função assíncrona é responsável por conectar ao MongoDB usando Mongoose.
+  
+  try {
+    await mongoose.connect(uri); 
+    //Essa linha tenta conectar ao MongoDB usando a string de conexão obtida anteriormente. O await é usado porque mongoose.connect retorna uma Promise (uma operação assíncrona) e queremos esperar até que a conexão seja estabelecida.
+    console.log('Conectado ao MongoDB com sucesso!');
+    
+    // Testar conexão enviando um ping
+    await mongoose.connection.db.command({ ping: 1 });
+    //Esta linha envia um comando de ping para o banco de dados MongoDB, confirmando que a conexão está ativa. É uma maneira de verificar a saúde da conexão.
+    console.log('Ping bem-sucedido ao MongoDB!');
+
+  } catch (err) {
+    console.error('Erro ao conectar ao MongoDB:', err);
+  }
+
+}
+
+// Chamar a função de conexão
+connectMongoDB();
+
+// Iniciar o servidor Express
+app.listen(port, () => { //Esta linha inicia o servidor Express, fazendo com que ele comece a escutar na porta definida. O callback fornece uma mensagem de log que confirma que o servidor está em execução.
+  console.log(`Servidor rodando na porta ${port}`);
+});
