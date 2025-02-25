49cba8f308873baa21ec4121ac81ebb95f880f8c
Rafael-Machado032 <rafaelmfds@gmail.com>
2025-02-24 22:10:22 -0300

    atualização

--- JavaScript Estudos/Projeto JavaScript 15 Tela Login/login.js
+++ JavaScript Estudos/Projeto JavaScript 15 Tela Login/login.js
@@ -1,22 +1,32 @@
 class Login {
 
+    static mat = null;
+    static pass = null;
     static logado = false;
     static matlogado = null;
     static nomelogado = null;
     static acessologado = null;
     static estilocss = null;
+    static callback_ok = null;
+    static callback_nook = null;
     static config = {
         cor: "#048",
         img: "./logo.jpg"
     };
-    static endpoint = "https://ea5e330f-3e67-46f7-96a3-4e29209807d2-00-1rhphb7s2ac4p.riker.replit.dev/"
+    
+    
 
-    static login = ( mat, pas, config = null ) => {
+    static login = ( callback_ok,callback_nook,config = null ) => {
         if (config!=null) {
-            this.config = config;
+            this.config = config; 
         }
+        this.callback_ok = () => {
+            callback_ok();
+        };
+        this.callback_nook = () => {
+            callback_nook();
+        };
         
-        this.endpoint += `?matricula=${mat}&senha=${pas}`;
         this.estilocss =
             ".fundologin{ display: flex; justify-content: center; align-items: center; width: 100%; height: 100vh; position: absolute; top: 0px; left: 0px; background-color: rgba(0, 0, 0, 0.75); box-sizing: border-box; }" +
             ".baselogin{ display: flex; justify-content: center; align-items: stretch;width: 50%; box-sizing: inherit; }" +
@@ -27,10 +37,10 @@ class Login {
             ".campologin label{ font-size: 18px; }"+
             ".campologin input{ font-size: 18px; padding: 5px;background-color: #fff; border-radius: 5px; }" +
             ".botoeslogin{ display: flex; justify-content: space-around; align-items: center; width: 100%; box-sizing: inherit; }" +
-            `.botoeslogin button{ cursor: pointer; background-color: #${this.config}; color: #fff; border-radius: 5px; padding: 10px; width: 100px; box-sizing: inherit; }`
-        
+            `.botoeslogin button{ cursor: pointer; background-color: ${this.config.cor}; color: #fff; border-radius: 5px; padding: 10px; width: 100px; box-sizing: inherit; }`
+            
             const styleestilo = document.createElement("style");
-            styleestilo.setAttribute("id","id_estilologin");
+            styleestilo.setAttribute("id","id_estiloLogin");
             styleestilo.setAttribute("rel","stylesheet");
             styleestilo.setAttribute("type","text/css");
             styleestilo.innerHTML = this.estilocss;
@@ -91,38 +101,76 @@ class Login {
             btn_login.setAttribute("id","btn_login");
             btn_login.innerHTML = "Login"
             botoeslogin.appendChild(btn_login);
-           
+            btn_login.addEventListener("click",(evt)=>{
+                this.verificaLogin();
+            })
+            
             const btn_cancelar = document.createElement("button");
             btn_cancelar.setAttribute("id","btn_login");
             btn_cancelar.innerHTML = "Cancelar"
             botoeslogin.appendChild(btn_cancelar);
+            btn_cancelar.addEventListener("click",(evt) => {
+                this.fechar();
+            })
 
             const logologin = document.createElement("div");
             logologin.setAttribute("id","logologin");
             logologin.setAttribute("class","logologin");
             baselogin.appendChild(logologin);
-           
+        
             const img_logologin = document.createElement("img");
             img_logologin.setAttribute("src",this.config.img);
             img_logologin.setAttribute("title","Tela Login");
             logologin.appendChild(img_logologin);
-           
-            
-        //     fetch(this.endpoint)
-        //     .then(res => res.json())
-        //     .then(res => {
-        //         if (res) {
-        //             this.logado = true;
-        //             this.matlogado = mat;
-        //             this.nomelogado = res.nome;
-        //             this.acessologado = res.acesso;
-        //             console.log(res);
-        //         }else{
-        //             console.log("Usuário não encontrado");
-        //         }
+    
+    }
 
-        //     })
-        // }
+    static verificaLogin = () => {
+        
+        const mat = document.querySelector("#f_username").value;
+        const pas = document.querySelector("#f_senha").value;
+        
+        const endpoint = `https://ea5e330f-3e67-46f7-96a3-4e29209807d2-00-1rhphb7s2ac4p.riker.replit.dev/?matricula=${mat}&senha=${pas}`
+        
+            fetch(endpoint)
+            .then(res => res.json())
+            .then(res => {
+            
+                if (res) {
+                    this.logado = true;
+                    this.matlogado = mat;
+                    this.nomelogado = res.nome;
+                    this.acessologado = res.acesso;
+                    console.log(res);
+                    this.callback_ok();
+                    this.fechar();
+                }else{
+                    this.logado = false;
+                    this.matlogado = null;
+                    this.nomelogado = null;
+                    this.acessologado = null;
+                    this.callback_nook();
+                }
+
+            })
+        
+
+
+
+
+
+            // if (mat=="123"&& pas=="123") {
+            //     return true;
+            // }else{
+            //     return false;
+            // }
+    }
+    static fechar = () => {
+    
+        const fundologin = document.querySelector("#fundologin");
+        fundologin.remove();
+        const id_estiloLogin = document.querySelector("#id_estiloLogin");
+        id_estiloLogin.remove();
     }
 }
 export { Login }
