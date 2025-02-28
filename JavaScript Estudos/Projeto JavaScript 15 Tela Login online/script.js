cdf1e0717d11a8cb461f33c6ed5e18b3f8f915e1
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2025-02-27 22:07:15 -0300

    a

--- JavaScript Estudos/Projeto JavaScript 15 Tela Login online/script.js
+++ JavaScript Estudos/Projeto JavaScript 15 Tela Login online/script.js
@@ -18,7 +18,7 @@ const callback_nook = () => {
 }
 const configlogin = {
     cor: "#800",
-    img: "https://www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+    img: "./logo.jpg",
     endpoint: "https://ea5e330f-3e67-46f7-96a3-4e29209807d2-00-1rhphb7s2ac4p.riker.replit.dev"
 }
 
