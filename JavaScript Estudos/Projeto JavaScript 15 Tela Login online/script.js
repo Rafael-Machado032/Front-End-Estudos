60ced8869349808b0931cf01d1edcadee63304a4
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2025-02-27 21:34:35 -0300

    a

--- JavaScript Estudos/Projeto JavaScript 15 Tela Login online/script.js
+++ JavaScript Estudos/Projeto JavaScript 15 Tela Login online/script.js
@@ -1,5 +1,5 @@
 import { Login } from "./login.js";
-import { Cxmsg } from "../Projeto JavaScript 13 caixa de mensagem Personallizado com dois botoes/cxmsg.js";
+//import { Cxmsg } from "../Projeto JavaScript 13 caixa de mensagem Personallizado com dois botoes/cxmsg.js";
 
 const callback_ok = () => {
     alert("Login efetuado com sucesso!");
