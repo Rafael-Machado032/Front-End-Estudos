269add76b3d0cd8077f8337f463be2a1452b1d3b
Rafael-Machado032 <rafaelmfds@gmail.com>
2025-03-24 22:37:33 -0300

    a

--- JavaScript Estudos/Projeto JavaScripit Agenda/script.js
+++ JavaScript Estudos/Projeto JavaScripit Agenda/script.js
@@ -6,3 +6,37 @@ const btn_pesquisar = document.querySelector('#btn_pesquisar');
 const btn_gestao = document.querySelector('#btn_gestao');
 const btn_sobre = document.querySelector('#btn_sobre');
 const principal = document.querySelector('#principal');
+
+btn_home.addEventListener('click', (evt) => {
+    selecionarAba(evt.target, 'home.html');
+    
+});
+
+btn_novo.addEventListener('click', (evt) => {
+    selecionarAba(evt.target, 'novo.html');
+
+});
+
+btn_pesquisar.addEventListener('click', (evt) => {
+    selecionarAba(evt.target, 'pesquisar.html');
+    
+});
+
+btn_gestao.addEventListener('click', (evt) => {
+    selecionarAba(evt.target, 'gestao.html');
+    
+});
+
+btn_sobre.addEventListener('click', (evt) => {
+    selecionarAba(evt.target, 'sobre.html');
+    
+});
+
+const selecionarAba = (el,url) => {
+    const aba = [...document.querySelectorAll('.aba')];
+    aba.forEach((item) => {
+        item.classList.remove('selecionado');
+    });
+    el.classList.add('selecionado');
+    window.open(url, 'if_principal');
+}
