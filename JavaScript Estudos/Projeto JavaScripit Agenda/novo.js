dea2e86d1d7897fc7979b308f154c4ec8fb8b413
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2025-03-30 13:28:11 -0300

    a

--- JavaScript Estudos/Projeto JavaScripit Agenda/novo.js
+++ JavaScript Estudos/Projeto JavaScripit Agenda/novo.js
@@ -6,12 +6,6 @@ btn_salvar.addEventListener('click', function (evt) {
         "f_nome":f_nome.value,
         "f_telefone":f_telefone.value,
         "f_email":f_email.value,
-        "f_endereco":f_endereco.value,
-        "f_cidade":f_cidade.value,
-        "f_estado":f_estado.value,
-        "f_cep":f_cep.value,
-        "f_pais":f_pais.value,
-        "f_nascimento":f_nascimento.value,
         "f_obs":f_obs.value
     };
 
@@ -44,12 +38,6 @@ btn_cancelar.addEventListener('click', function (evt) {
     f_nome.value = '';
     f_telefone.value = '';
     f_email.value = '';
-    f_endereco.value = '';
-    f_cidade.value = '';
-    f_estado.value = '';
-    f_cep.value = '';
-    f_pais.value = '';
-    f_nascimento.value = '';
     f_obs.value = '';
     f_nome.focus();
 }
