5a684afdf16d9535293763c1ca4aa5b0fa942339
Rafael-Machado032 <rafaelmfds@gmail.com>
2025-04-13 14:35:46 -0300

    a

--- JavaScript Estudos/Projeto JavaScript Estoque/backoffice/index.js
+++ JavaScript Estudos/Projeto JavaScript Estoque/backoffice/index.js
@@ -1,7 +1,19 @@
 const btn_menupricipal = document.querySelector('.btn_menuprincipal');
 const menuprincipal = document.querySelector('.menuprincipal');
-const todosmenusprincipais = document.querySelectorAll('.btn_menuitem');
+const todosmenusprincipais = [...document.querySelectorAll('.btn_menuitem')];
+
 
 btn_menupricipal.addEventListener('click', () => {
     menuprincipal.classList.toggle('ocultar');
 });
+
+todosmenusprincipais.forEach((item) => {
+    item.addEventListener('click', () => {
+        menuprincipal.classList.add('ocultar');
+    });
+});
+
+
+
+
+
