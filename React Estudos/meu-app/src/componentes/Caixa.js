08d0b38ba84ea6f3bb5ee561a27c0073a4ae05fb
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2025-08-04 23:29:16 -0300

    a

---
+++ React Estudos/meu-app/src/componentes/Caixa.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Caixa(props) {
+    return (
+        <div className="caixa">
+            {props.children}
+            {props.children[0]}
+            <a href={props.site} target="_blank" rel="noopener noreferrer">
+                Acesse o site
+            </a>
+        </div>
+    );
+}
