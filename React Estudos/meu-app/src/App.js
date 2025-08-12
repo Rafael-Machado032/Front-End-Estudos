d50e93b44be8945c1b0c6c871df21389a1bad8aa
Rafael-Machado032 <rafaelmfds@gmail.com>
2025-08-11 23:44:52 -0300

    a

--- React Estudos/meu-app/src/App.js
+++ React Estudos/meu-app/src/App.js
@@ -16,33 +16,13 @@ import ElevacaoState from './App/ElevacaoState'; // Importando o componente Elev
 import ElevacaoStateHandle from './App/ElevacaoStateHandle'; // Importando o componente ElevacaoStateHandle
 import Contecao from './App/Contecao';
 import UseEffect from './App/UseEffect';
+import LocalStorage from './App/LocalStorage'; // Importando o componente LocalStorage
 
 
 
 
 export default function App() {
-
-
-  localStorage.setItem('nome', 'Rafael'); // Armazenando um valor no localStorage
-  localStorage.getItem('nome'); // Recuperando o valor do localStorage
-  localStorage.removeItem('nome'); // Removendo o valor do localStorage
-
-  const [nomeArmazenado, setNomeArmazenado] = React.useState(''); // Estado para armazenar o nome a ser salvo no localStorage
-  const armazenar = (chave,valor) => {
-    localStorage.setItem(chave, valor); // Função para armazenar um valor no localStorage
-  }
-
-  const consultar = (chave) => {
-    alert(localStorage.getItem(chave)); // Função para consultar um valor no localStorage
-  }
-
-  const apagar = (chave) => {
-    localStorage.removeItem(chave); // Função para apagar um valor do localStorage
-  }
-
-
   return (
-
     <> {/* Importando o componentes */}
 
       <Componentes />
@@ -60,23 +40,7 @@ export default function App() {
       <ElevacaoStateHandle />
       <Contecao />
       <UseEffect />
-
-
-      
-
-      
-      
-    
-      
-
-      
-        
-      <p>Armazenar nome: </p>
-      <input type="text" value={nomeArmazenado} onChange={(e) => setNomeArmazenado(e.target.value)} />
-      <button onClick={() => armazenar('nome', nomeArmazenado)}>Salvar</button>
-      <button onClick={() => consultar('nome')}>Consultar</button>
-      <button onClick={() => apagar('nome')}>Apagar</button>
-      
+      <LocalStorage />
 
     </>
   );
