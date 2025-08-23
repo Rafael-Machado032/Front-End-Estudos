78d31ce45f13ab69d2e44b017e6e1dde347b329e
Rafael-Machado032 <rafaelmfds@gmail.com>
2025-08-23 16:24:23 -0300

    A

--- React Estudos/meu-app/src/App/CalculadoraMatematica.js
+++ React Estudos/meu-app/src/App/CalculadoraMatematica.js
@@ -29,7 +29,7 @@ export default function CalculadoraMatematica() {
     const addDigitoTel = (d) => {
         if ((d === "+" || d === "-" || d === "*" || d === "/") && operado) {
             setOperado(false);
-            setValorTela(valorTela + d);
+            setValorTela(resultado + d);
             return;
         }
         if (operado) {
@@ -41,39 +41,75 @@ export default function CalculadoraMatematica() {
         setValorTela(valorDigitadoTela);
     }
 
-    const limparMemoria = () =>{
+    const limparMemoria = () => {
         setValorTela("");
         setOperado(false);
         setAcumulador(0);
         setResultado(0);
         return Tela("", 0);
     }
+    const Operacao = (op) => {
+        if (op ==="bs") {
+            let vtela = valorTela;
+            vtela = vtela.substring(0, vtela.length - 1);
+            setValorTela(vtela);
+            setOperado(false);
+            return
+        }
+        try {//como vou fazer uma operação com calculo e pode dar erro
+            const r = eval(valorTela);//eval é um função que avalia uma string como código JavaScript
+            setAcumulador(r);
+            setResultado(r);
+            setOperado(true);
+        } catch (e) {
+            setResultado("ERRO");
+        }
+    }
 
     //Estilo
+
+    const cssContainer = {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "300px",
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        padding: "10px"
+    };
     const cssTela = {
         fontSize: 30,
         color: "#000",
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "column",
         width: "100%",
-        marginTop: "10px"
-    }
+        marginTop: "10px",
+        padding: "10px",
+    };
     const cssOperacao = {
-        fontSize: 25,
+        fontSize: 20,
         color: "#333",
         display: "flex",
-        justifyContent: "space-between",
         width: "100%",
-        marginTop: "10px"
+        height: "10px",
     };
     const cssResultado = {
         fontSize: 30,
         color: "#000",
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "right",
+        width: "100%",
+        
+    };
+    const cssBotoes = {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
         width: "100%",
         marginTop: "10px"
     };
+    
+    
     const cssBtn = {
         fontSize: 20,
         color: "#fff",
@@ -88,6 +124,41 @@ export default function CalculadoraMatematica() {
     return (
         <div>
             <h1>Calculadora Matemática</h1>
+            <div style={cssContainer}>
+                {Tela(valorTela, resultado)}
+                <div style={cssBotoes}>
+                    {Btn("C", () => limparMemoria())}
+                    {Btn("(", () => addDigitoTel("("))}
+                    {Btn(")", () => addDigitoTel(")"))}
+                    {Btn("/", () => addDigitoTel("/"))}
+                </div>
+                <div style={cssBotoes}>
+                    {Btn("7", () => addDigitoTel("7"))}
+                    {Btn("8", () => addDigitoTel("8"))}
+                    {Btn("9", () => addDigitoTel("9"))}
+                    {Btn("*", () => addDigitoTel("*"))}
+                </div>
+                <div style={cssBotoes}>
+                    {Btn("4", () => addDigitoTel("4"))}
+                    {Btn("5", () => addDigitoTel("5"))}
+                    {Btn("6", () => addDigitoTel("6"))}
+                    {Btn("-", () => addDigitoTel("-"))}
+                </div>
+                <div style={cssBotoes}>
+                    {Btn("1", () => addDigitoTel("1"))}
+                    {Btn("2", () => addDigitoTel("2"))}
+                    {Btn("3", () => addDigitoTel("3"))}
+                    {Btn("+", () => addDigitoTel("+"))}
+                </div>
+                <div style={cssBotoes}>
+                    {Btn("0", () => addDigitoTel("0"))}
+                    {Btn(".", () => addDigitoTel("."))}
+                    {Btn("<", () => Operacao("bs"))}
+                    {Btn("=", () => Operacao("="))}
+                    
+                </div>
+                
+            </div>
         </div>
     );
 }
