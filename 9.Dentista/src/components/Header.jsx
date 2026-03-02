e93b536cb6884312a8fa89dbf6f6de2862ed3538
Rafael-Machado032 <rafaelmfds@gmail.com>
2026-03-02 14:43:06 -0300

    a

--- 9.Dentista/src/components/Header.jsx
+++ 9.Dentista/src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+
 
 export default function Header() {
   return (
-    <header className='w-full'>
-      <div></div>
-
+    <header className='w-full max-w-4xl mx-auto py-6 flex justify-between text-white'>
+      <div style={{backgroundImage: `url(${logo})`}} className="bg-no-repeat bg-center bg-contain w-40 h-19"></div>
+      <nav>
+        <ul className='flex gap-8'>
+          <li><a href="#">Invisalign</a>
+          <span></span></li>
+          <li><a href="#">Benefícios</a>
+          <span></span></li>
+          <li><a href="#">Serve para mim?</a>
+          <span></span></li>
+          <li><a href="#">Contato</a>
+          <span></span></li>
+        </ul>
+      </nav>
+      <h2 className='text-2xl'>(48) 3222-6591</h2>
     </header>
   )
 }
